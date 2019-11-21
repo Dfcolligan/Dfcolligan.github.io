81eecd96fc17e8010d893e63a98344aa18c4217f
Dfcolligan <dfcolligan@gmail.com>
2019-11-21 19:36:22 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -23,16 +23,27 @@ var level01 = function (window) {
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // BEGIN EDITING YOUR CODE HERE
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
-        myObstacle.x = 400;
-        myObstacle.y = 100;
-        game.addGameItem(myObstacle);    
-
+         function createSawBlade(x,y) {
+             
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+            myObstacle.x = x;
+            myObstacle.y = y;
+            
+            game.addGameItem(myObstacle);    
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+            myObstacle.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;            
+        }
+        
+        createSawBlade(500, groundY - 50);
+        createSawBlade(1000, groundY - 90);
+        createSawBlade(1200, groundY - 30);
     }
 };
 
